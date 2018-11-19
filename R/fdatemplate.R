14f59845e9add752ba3787a7dc9fa392a12d56f4
Thompson <LXT@fda.gov>
2018-11-18 20:35:22 -0500

    commit all

---
+++ R/fdatemplate.R
@@ -0,0 +1,8 @@
+#' package to distribute fdatemplate
+#'
+#' Open a new rmarkdown file.  Select the "fdatemplate" from the list of templates.
+#'
+#'
+#' @docType package
+#' @name fdatemplate
+NULL
